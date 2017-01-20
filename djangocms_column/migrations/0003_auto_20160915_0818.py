0a506c0624a51bc6d0a88465e57cc92bf814eb41
Corey Oordt <coreyoordt@gmail.com>
2017-01-20 13:34:31 -0600

    Change source of migration 0003

--- djangocms_column/migrations/0003_auto_20160915_0818.py
+++ djangocms_column/migrations/0003_auto_20160915_0818.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('djangocms_column', '0001_initial'),
+        ('djangocms_column', '0002_add_gutter_width.py'),
     ]
 
     operations = [
